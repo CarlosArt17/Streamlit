c79ff3a13b45b207b25130acc2fe429cdf94fe05
Carlos Arturo <zs20006730@estudiantes.uv.mx>
2023-03-07 09:46:39 -0600

    proyecto citybike-app

--- citibike-app.py
+++ citibike-app.py
@@ -43,6 +43,6 @@ if st.sidebar.checkbox('Recorridos por hora'):
 hour_to_filter = sidebar.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.subheader('Mapa de recorridos iniciado a las %s:00' % hour_to_filter)
 st.map(filtered_data)
 
